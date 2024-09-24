61be31ef88337d1f5140ad143bd00cf7eb81b029
Hamza <hamzaveliyev00@gmail.com>
2024-09-24 11:53:36 +0400

    24.09

---
+++ shablonstrukturi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+template <typename T1,typename T2,typename T3>
+struct Triple{
+	T1 first;
+	T2 second;
+	T3 third;
+};
+int main(){
+	Triple<int,int,int> point {-1,3,2};
+	Triple<std::string,std::string,int> wordPairsfreq{"hello","world",42};
+	return 0;
+}  
